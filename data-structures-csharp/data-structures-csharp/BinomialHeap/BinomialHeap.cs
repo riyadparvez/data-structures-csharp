29e98705318a9f99117e587e551502c89766c0ae
Riyad Parvez <social.riyad@gmail.com>
2013-02-12 17:30:24 +0600

    Add: Binomial Heap

---
+++ data-structures-csharp/data-structures-csharp/BinomialHeap/BinomialHeap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+namespace DataStructures.BinomialHeapSpace
+{
+    [Serializable]
+    public class BinomialHeap<T>
+        where T : IComparable<T>
+    {
+        private Node<T> root;
+
+        private Node<T> AddSubTree(Node<T> root)
+        {
+            Contract.Requires<ArgumentNullException>(root != null);
+        }
+
+        public Node<T> Merge(BinomialHeap<T> otherHeap)
+        {
+            Contract.Requires<ArgumentNullException>(otherHeap != null);
+
+            if (root.Value.CompareTo(otherHeap.root.Value) <= 0)
+            {
+                return AddSubTree(otherHeap.root);
+            }
+            else
+            {
+                return otherHeap.AddSubTree(root);
+            }
+        }
+    }
+}
---
+++ data-structures-csharp/data-structures-csharp/BinomialHeap/Node.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+namespace DataStructures.BinomialHeapSpace
+{
+    /// <summary>
+    /// Node of Heap
+    /// </summary>
+    /// <typeparam name="T">Data type</typeparam>
+    [Serializable]
+    public class Node<T>
+        where T : IComparable<T>
+    {
+        private T val;
+
+        public T Value
+        {
+            get { return val; }
+            internal set
+            {
+                Contract.Requires<ArgumentNullException>(value != null);
+                val = value;
+            }
+        }
+        public int Height { get; internal set; }
+        internal Node<T> Parent { get; set; }
+        internal Node<T> Left { get; set; }
+        internal Node<T> Right { get; set; }
+
+        public Node(T val, Node<T> parent)
+        {
+            Contract.Requires<ArgumentNullException>(val != null);
+
+            this.val = val;
+            Parent = parent;
+            Left = null;
+            Right = null;
+        }
+
+        public bool Equals(Node<T> otherNode)
+        {
+            if (otherNode == null)
+            {
+                return false;
+            }
+            return val.Equals(otherNode.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Node<T> otherNode = obj as Node<T>;
+            if (otherNode == null)
+            {
+                return false;
+            }
+            return val.Equals(otherNode.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                // Suitable nullity checks etc, of course :)
+                hash = hash * 23 + val.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
