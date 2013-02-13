09814cf32980abe1ea1377eea4091129bc98d830
Riyad Parvez <social.riyad@gmail.com>
2013-02-13 19:05:54 +0600

    Add: LinearProbingDictionary

---
+++ data-structures-csharp/data-structures-csharp/Hash/LinearProbingDictionary.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+
+namespace DataStructures.HashSpace
+{
+    /// <summary>
+    /// A hashtable which uses user specified step size probing
+    /// </summary>
+    /// <typeparam name="Tkey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    [Serializable]
+    public class LinearProbingDictionary<Tkey, TValue> : IDictionary<Tkey, TValue>
+    {
+        private const int DefaultCapacity = 1023;
+        private int capacity;
+        private readonly int stepSize;
+        private int count;
+        private bool[] isFilled;
+        private Pair<Tkey, TValue>[] pairs;
+
+        public ICollection<Tkey> Keys
+        {
+            get { return pairs.Select(p => p.Key).ToList().AsReadOnly(); }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { return pairs.Select(p => p.Value).ToList().AsReadOnly(); }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+        public LinearProbingDictionary(int capacity, int stepSize)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(capacity > 0);
+            Contract.Requires<ArgumentOutOfRangeException>(stepSize > 0);
+
+            this.capacity = capacity;
+            this.stepSize = stepSize;
+            pairs = new Pair<Tkey, TValue>[capacity];
+            isFilled = new bool[capacity];
+        }
+
+        public LinearProbingDictionary(int stepSize)
+            : this(DefaultCapacity, stepSize)
+        {
+        }
+
+        public void Add(Tkey key, TValue value)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns index of th item
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>-1 if not present</returns>
+        private int GetIndex(Tkey key)
+        {
+            if (key == null)
+            {
+                return -1;
+            }
+
+            int index = key.GetHashCode() % capacity;
+            int currentIndex = index;
+            do
+            {
+                if (pairs[currentIndex] == null)
+                {
+                    return -1;
+                }
+                if (pairs[currentIndex].Key.Equals(key))
+                {
+                    return currentIndex;
+                }
+                else
+                {
+                    currentIndex = (currentIndex + stepSize) % capacity;
+                }
+            } while (currentIndex != index);
+            return -1;
+        }
+
+        public bool ContainsKey(Tkey key)
+        {
+            if (GetIndex(key) == -1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool Remove(Tkey key)
+        {
+            Contract.Requires<ArgumentNullException>(key != null, "key");
+            int index;
+            if ((index = GetIndex(key)) != -1)
+            {
+                pairs[index] = null;
+                count--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool TryGetValue(Tkey key, out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue this[Tkey key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(KeyValuePair<Tkey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            isFilled = Array.ConvertAll<bool, bool>(isFilled, b => b = false);
+            pairs = Array.ConvertAll<Pair<Tkey, TValue>, Pair<Tkey, TValue>>(pairs, pairs => pairs = null);
+        }
+
+        public bool Contains(KeyValuePair<Tkey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<Tkey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<Tkey, TValue> item)
+        {
+            if (!ContainsKey(item.Key))
+            {
+                return true;
+            }
+
+        }
+
+        public IEnumerator<KeyValuePair<Tkey, TValue>> GetEnumerator()
+        {
+            foreach (var pair in pairs)
+            {
+                yield return new KeyValuePair<Tkey, TValue>(pair.Key, pair.Value);
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class Pair<TKey, TValue>
+        {
+            public Tkey Key { get; set; }
+            public TValue Value { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                {
+                    return false;
+                }
+
+                return Key.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                int hash = 31;
+                unchecked
+                {
+                    hash = hash + 17 * Key.GetHashCode();
+                }
+                return hash;
+            }
+        }
+    }
+}
---
+++ data-structures-csharp/data-structures-csharp/Hash/LinkedHashMap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace DataStructures.HashSpace
+{
+    [Serializable]
+    public class LinkedHashMap<TKey, TValue>
+    {
+
+
+        private class Entry<TKey, TValue>
+        {
+            // These fields comprise the doubly linked list used for iteration.
+            Entry<TKey, TValue> before, after;
+
+            Entry(int hash, TKey key, TValue value, Entry<TKey, TValue> next)
+            {
+                super(hash, key, value, next);
+            }
+
+            /**
+             * Removes this entry from the linked list.
+             */
+            private void Remove()
+            {
+                before.after = after;
+                after.before = before;
+            }
+
+            /**
+             * Inserts this entry before the specified existing entry in the list.
+             */
+            private void InsertBefore(Entry<TKey, TValue> existingEntry)
+            {
+                after = existingEntry;
+                before = existingEntry.before;
+                before.after = this;
+                after.before = this;
+            }
+
+            /**
+             * This method is invoked by the superclass whenever the value
+             * of a pre-existing entry is read by Map.get or modified by Map.set.
+             * If the enclosing Map is access-ordered, it moves the entry
+             * to the end of the list; otherwise, it does nothing.
+             */
+            void recordAccess(LinkedHashMap<TKey, TValue> lm)
+            {
+                if (lm.accessOrder)
+                {
+                    lm.modCount++;
+                    Remove();
+                    InsertBefore(lm.header);
+                }
+            }
+        }
+    }
+}
