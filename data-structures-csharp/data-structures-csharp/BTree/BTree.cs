02d987058dd4c1519c5b4c8840cf5195af02dbf4
Riyad Parvez <social.riyad@gmail.com>
2013-02-25 18:54:47 +0600

    Add: Search in B Tree

--- data-structures-csharp/data-structures-csharp/BTree/BTree.cs
+++ data-structures-csharp/data-structures-csharp/BTree/BTree.cs
@@ -15,15 +15,66 @@ namespace DataStructures.BTreeSpace
         {
             get { return maximumChildrenPerNode; }
         }
-
+        public int Height { get; private set; }
         public int Count { get; private set; }
 
         [ContractInvariantMethod]
-        private void ObjectInvariant() 
+        private void ObjectInvariant()
         {
+            Contract.Invariant(root != null);
             Contract.Invariant(Count >= 0);
+            Contract.Invariant(Height > 0);
         }
 
-        public 
+        public BTree()
+        {
+            root = new Node<TKey, TValue>(0);
+        }
+
+        private TValue Search(Node<TKey, TValue> node, TKey key, int ht)
+        {
+            Contract.Requires<ArgumentNullException>(node != null);
+            Contract.Requires<ArgumentNullException>(key != null);
+            Contract.Requires<ArgumentOutOfRangeException>(ht >= 0);
+
+            Entry<TKey, TValue>[] children = node.Children;
+
+            // leaf node i.e. external node
+            if (ht == 0)
+            {
+                //search all the keys in leaf nodes
+                for (int j = 0; j < node.maximumNumberOfChildren; j++)
+                {
+                    if (Equals(key, children[j].Key))
+                    {
+                        return children[j].Value;
+                    }
+                }
+            }
+            // internal node
+            else
+            {
+                //search all the children
+                for (int j = 0; j < node.maximumNumberOfChildren; j++)
+                {
+                    if (j + 1 == node.maximumNumberOfChildren || Less(key, children[j + 1].Key))
+                    {
+                        return Search(children[j].ChildNode, key, ht - 1);
+                    }
+                }
+            }
+            //key isn't found
+            return default(TValue);
+        }
+
+        private bool Less(IComparable<TKey> k1, TKey k2)
+        {
+            return k1.CompareTo(k2) < 0;
+        }
+
+        private bool Equals(IComparable<TKey> k1, TKey k2)
+        {
+            return k1.CompareTo(k2) == 0;
+        }
     }
 }
--- data-structures-csharp/data-structures-csharp/BTree/Entry.cs
+++ data-structures-csharp/data-structures-csharp/BTree/Entry.cs
@@ -6,19 +6,24 @@ namespace DataStructures.BTreeSpace
     public partial class BTree<TKey, TValue>
         where TKey : IComparable<TKey>
     {
+        /// <summary>
+        /// Every entry in children contains either a key value pair or link to next child
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
         [Serializable]
         private sealed class Entry<TKey, TValue>
             where TKey : IComparable<TKey>
         {
             public TKey Key { get; set; }
             public TValue Value { get; set; }
-            public Node<TKey, TValue> Next { get; set; }
+            public Node<TKey, TValue> ChildNode { get; set; }
 
             public Entry(TKey key, TValue value, Node<TKey, TValue> next)
             {
                 this.Key = key;
                 this.Value = value;
-                this.Next = next;
+                this.ChildNode = next;
             }
         }
     }
