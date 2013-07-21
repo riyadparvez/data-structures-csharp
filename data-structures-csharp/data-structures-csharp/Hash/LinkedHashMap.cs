8cc8d774e7f613a7b6037a9a99af6a701bffa758
Riyad Parvez <social.riyad@gmail.com>
2013-07-21 21:20:57 +0600

    added idictionary interface to linkedhasmap

--- data-structures-csharp/data-structures-csharp/Hash/LinkedHashMap.cs
+++ data-structures-csharp/data-structures-csharp/Hash/LinkedHashMap.cs
@@ -1,21 +1,144 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace DataStructures.HashSpace
 {
     [Serializable]
-    public class LinkedHashMap<TKey, TValue>
+    public class LinkedHashMap<TKey, TValue> : IDictionary<TKey, TValue>
     {
+        private Pair<TKey, TValue> header;
 
-
-        private class Entry<TKey, TValue>
+        void Initialize()
         {
-            // These fields comprise the doubly linked list used for iteration.
-            Entry<TKey, TValue> before, after;
+            header = new Pair<TKey, TValue>(-1, default(TKey), default(TValue), null);
+            header.previous = header.next = header;
+        }
 
-            Entry(int hash, TKey key, TValue value, Entry<TKey, TValue> next)
+        public bool ContainsValue(TValue value)
+        {
+            // Overridden to take advantage of faster iterator
+            if (value == null)
             {
-                super(hash, key, value, next);
+                for (var e = header.next; e != header; e = e.next)
+                {
+                    if (e.value == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                for (var e = header.next; e != header; e = e.next)
+                {
+                    if (value.Equals(e.value))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        private class Pair<TKey, TValue>
+        {
+            public TKey key;
+            public TValue value;
+            // These fields comprise the doubly linked list used for iteration.
+            public Pair<TKey, TValue> previous;
+            public Pair<TKey, TValue> next;
+
+            public Pair(int hash, TKey key, TValue value, Pair<TKey, TValue> next)
+            {
+                this.key = key;
+                this.value = value;
             }
 
             /**
@@ -23,19 +146,19 @@ namespace DataStructures.HashSpace
              */
             private void Remove()
             {
-                before.after = after;
-                after.before = before;
+                previous.next = next;
+                next.previous = previous;
             }
 
             /**
              * Inserts this entry before the specified existing entry in the list.
              */
-            private void InsertBefore(Entry<TKey, TValue> existingEntry)
+            private void InsertBefore(Pair<TKey, TValue> existingEntry)
             {
-                after = existingEntry;
-                before = existingEntry.before;
-                before.after = this;
-                after.before = this;
+                next = existingEntry;
+                previous = existingEntry.previous;
+                previous.next = this;
+                next.previous = this;
             }
 
             /**
@@ -54,5 +177,6 @@ namespace DataStructures.HashSpace
                 }
             }
         }
+
     }
 }
