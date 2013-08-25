8b967ee3b4d48efd8babae3a00efb7e39273b346
Riyad Parvez <social.riyad@gmail.com>
2013-08-25 06:51:29 +0600

    improved encapsulation

--- data-structures-csharp/data-structures-csharp/FrequencyList/Node.cs
+++ data-structures-csharp/data-structures-csharp/FrequencyList/Node.cs
@@ -3,51 +3,53 @@ using System.Diagnostics.Contracts;
 
 namespace DataStructures.FrequencyListSpace
 {
-    [Serializable]
-    public class Node<T>
-        where T : IComparable<T>
+    public partial class FrequencyList<T>
     {
-        public int AccessCount { get; set; }
-        public T Data { get; set; }
-        public Node<T> Previous { get; set; }
-        public Node<T> Next { get; set; }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
+        [Serializable]
+        public class Node<T>
+            where T : IComparable<T>
         {
-            Contract.Invariant(AccessCount >= 0);
-        }
+            public int AccessCount { get; set; }
+            public T Data { get; set; }
+            public Node<T> Previous { get; set; }
+            public Node<T> Next { get; set; }
 
-        public Node()
-        {
-        }
-
-        public Node(T data)
-        {
-            Contract.Requires<ArgumentNullException>(data != null);
-            Data = data;
-        }
-
-
-        public override int GetHashCode()
-        {
-            unchecked
+            [ContractInvariantMethod]
+            private void ObjectInvariant()
             {
-                int hash = 17;
-                hash = 23 * Data.GetHashCode();
-                return hash;
+                Contract.Invariant(AccessCount >= 0);
             }
-        }
 
-
-        public override bool Equals(object obj)
-        {
-            Node<T> otherObject = obj as Node<T>;
-            if (otherObject == null)
+            public Node()
             {
-                return false;
             }
-            return Data.Equals(otherObject.Data);
+
+            public Node(T data)
+            {
+                Contract.Requires<ArgumentNullException>(data != null);
+                Data = data;
+            }
+
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    int hash = 17;
+                    hash = 23 * Data.GetHashCode();
+                    return hash;
+                }
+            }
+
+            public override bool Equals(object obj)
+            {
+                Node<T> otherObject = obj as Node<T>;
+                if (otherObject == null)
+                {
+                    return false;
+                }
+                return Data.Equals(otherObject.Data);
+            }
         }
     }
 }
