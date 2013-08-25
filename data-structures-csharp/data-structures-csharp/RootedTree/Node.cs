48fb45af6b1dde963def5c74e63dc41273212258
Riyad Parvez <social.riyad@gmail.com>
2013-08-25 06:39:24 +0600

    improved encapsulation

--- data-structures-csharp/data-structures-csharp/RootedTree/Node.cs
+++ data-structures-csharp/data-structures-csharp/RootedTree/Node.cs
@@ -3,70 +3,73 @@ using System.Diagnostics.Contracts;
 
 namespace DataStructures.RootedTreeSpace
 {
-    /// <summary>
-    /// Node of rooted tree
-    /// </summary>
-    /// <typeparam name="T">Data type</typeparam>
-    [Serializable]
-    public class Node<T>
-        where T : IComparable<T>, IEquatable<T>
+    public partial class RootedTree<T>
     {
-        public readonly T data;
-
-        public T Data
+        /// <summary>
+        /// Node of rooted tree
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        [Serializable]
+        private class Node<T>
+            where T : IComparable<T>, IEquatable<T>
         {
-            get { return data; }
-        }
-        public int Height { get; internal set; }
-        internal Node<T> Parent { get; set; }
-        internal Node<T> Left { get; set; }
-        internal Node<T> Right { get; set; }
+            public readonly T data;
 
-        public Node<T> Root { get; set; }
-
-        [ContractInvariantMethod]
-        private void ObjectInvariant()
-        {
-            Contract.Invariant(Root != null);
-        }
-
-        public Node(T data, Node<T> parent)
-        {
-            Contract.Requires<ArgumentNullException>(parent != null);
-
-            this.data = data;
-            Parent = parent;
-            Left = null;
-            Right = null;
-        }
-
-        public bool Equals(Node<T> otherNode)
-        {
-            if (otherNode == null)
+            public T Data
             {
-                return false;
+                get { return data; }
             }
-            return data.Equals(otherNode.Data);
-        }
+            public int Height { get; set; }
+            public Node<T> Parent { get; set; }
+            public Node<T> Left { get; set; }
+            public Node<T> Right { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            Node<T> otherNode = obj as Node<T>;
-            if (otherNode == null)
+            public Node<T> Root { get; set; }
+
+            [ContractInvariantMethod]
+            private void ObjectInvariant()
             {
-                return false;
+                Contract.Invariant(Root != null);
             }
-            return Data.Equals(otherNode.Data);
-        }
 
-        public override int GetHashCode()
-        {
-            unchecked // Overflow is fine, just wrap
+            public Node(T data, Node<T> parent)
             {
-                int hash = 17;
-                // Suitable nullity checks etc, of course :)
-                hash = hash * 23 + data.GetHashCode();
-                return hash;
+                Contract.Requires<ArgumentNullException>(parent != null);
+
+                this.data = data;
+                Parent = parent;
+                Left = null;
+                Right = null;
+            }
+
+            public bool Equals(Node<T> otherNode)
+            {
+                if (otherNode == null)
+                {
+                    return false;
+                }
+                return data.Equals(otherNode.Data);
+            }
+
+            public override bool Equals(object obj)
+            {
+                Node<T> otherNode = obj as Node<T>;
+                if (otherNode == null)
+                {
+                    return false;
+                }
+                return Data.Equals(otherNode.Data);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked // Overflow is fine, just wrap
+                {
+                    int hash = 17;
+                    // Suitable nullity checks etc, of course :)
+                    hash = hash * 23 + data.GetHashCode();
+                    return hash;
+                }
             }
         }
     }
