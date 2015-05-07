cf65771eb546fe91572114d155c5492f3b2fda60
MARCO PIEROBON <marcopierobon@outlook.com>
2015-05-07 21:12:06 +0200

    Added unit tests to SortedList

--- data-structures-csharp/data-structures-csharp/List/SortedList.cs
+++ data-structures-csharp/data-structures-csharp/List/SortedList.cs
@@ -21,7 +21,8 @@ namespace DataStructures.ListSpace
 
         bool ICollection<T>.Remove(T item)
         {
-            throw new NotImplementedException();
+            var success = ((ICollection<T>)list).Remove(item);
+            return success;
         }
 
         public int Count
@@ -36,7 +37,7 @@ namespace DataStructures.ListSpace
         private void ObjectInvariant()
         {
             Contract.Invariant(Count >= 0);
-            Contract.Invariant(Capacity > 0);
+            Contract.Invariant(Capacity >= 0);
         }
 
         public SortedList() 
@@ -63,31 +64,42 @@ namespace DataStructures.ListSpace
         {
             Contract.Requires<ArgumentNullException>(element != null);
 
-            int count = 0;
-            for (int i = 0; i < list.Count; i++, count++)
-            {
-                if (element.CompareTo(list[i]) < 0)
+            var count = 0;
+            //search for the previous element only when there are other elements in the list
+            if(list.Count>0){
+                for (var i = 0; i < list.Count; i++, count++)
                 {
-                    break;
+                    if (element.CompareTo(list[i]) <= 0)
+                    {
+                        count = i;
+                        break;
+                    }
                 }
+                if(count>0)
+                    Contract.Assert(list[count-1].CompareTo(element) <= 0);
             }
-            Contract.Assert(list[count - 1].CompareTo(element) < 0);
             list.Insert(count, element);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            list = new List<T>();
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            return list.Contains(item);
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            Contract.Requires<ArgumentNullException>(array != null, "array is null");
+            Contract.Requires<ArgumentOutOfRangeException>(arrayIndex >= 0, "arrayIndex less than 0");
+
+            for (var i = arrayIndex; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
         }
 
         public void Remove(T element)
@@ -99,14 +111,14 @@ namespace DataStructures.ListSpace
 
         public void CopyTo(Array array, int index)
         {
-            Contract.Requires<ArgumentNullException>(array != null, "array");
-            Contract.Requires<ArgumentOutOfRangeException>(index >= 0, "index");
-            Contract.Requires<ArgumentException>(array.Length < Count);
+            Contract.Requires<ArgumentNullException>(array != null, "array is null");
+            Contract.Requires<ArgumentOutOfRangeException>(index >= 0, "arrayIndex less than 0");
+            Contract.Requires<ArgumentException>(array.Length < Count, "array not big enough");
 
             int i = index;
             foreach (T element in list)
             {
-                array.SetValue(element, i);
+                array.SetValue(element, i++);
             }
         }
 
---
+++ data-structures-csharp/data-structures-csharpTests/List/SortedListTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures.ListSpace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.SortedList.Tests
+{
+    [TestClass()]
+    public class SortedListTests
+    {
+        private SortedList<int> _sortedList = new SortedList<int>();
+        private readonly int[] _nodes = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        [TestInitialize, TestCleanup]
+        protected void UnLoadInput()
+        {
+            _sortedList = _sortedList = new SortedList<int>();
+        }
+
+        protected void LoadInput()
+        {
+            foreach (var node in _nodes)
+            {
+                _sortedList.Add(node);
+            }
+        }
+
+        [TestMethod]
+        public void AddElementTest()
+        {
+            const int elementToAdd = 1;
+            _sortedList.Add(elementToAdd);
+            const int expectedSize = 1;
+            Assert.AreEqual(expectedSize, _sortedList.Count, "Size do not match the expected value after one insert");
+        }
+
+        [TestMethod]
+        public void AddSameElementTest()
+        {
+            const int elementToAdd = 1;
+            _sortedList.Add(elementToAdd);
+            _sortedList.Add(elementToAdd);
+            const int expectedSize = 2;
+            Assert.AreEqual(expectedSize, _sortedList.Count, "Size do not match the expected value after double insert");
+        }
+
+        [TestMethod]
+        public void AddElementsOrderedTest()
+        {
+            for (var i = _nodes.Length - 1; i >= 0; i--)
+            {
+                _sortedList.Add(i);
+            }
+            const int firstIndexToCheck = 0;
+            const int secondIndexToCheck = 1;
+            var size = _sortedList.Count;
+            var firstElement = _sortedList.ElementAt(firstIndexToCheck);
+            var secondElement = _sortedList.ElementAt(secondIndexToCheck);
+
+            Assert.IsTrue(firstElement <= secondElement, "List not sorted");
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            LoadInput();
+            _sortedList.Clear();
+            const int expectedCount = 0;
+            Assert.AreEqual(expectedCount, _sortedList.Count, "Cleared list has size <> 0");
+        }
+
+        [TestMethod]
+        public void ContainTest()
+        {
+            const int elementToAdd = 1;
+            _sortedList.Add(elementToAdd);
+            Assert.IsTrue(_sortedList.Contains(elementToAdd), "List does not contain the added element");
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            const int element = 1;
+            _sortedList.Add(element);
+            _sortedList.Remove(element);
+            Assert.IsFalse(_sortedList.Contains(element), "List still contains the rimoved element");
+        }
+
+
+        [TestMethod]
+        public void RemoveNonAddedTest()
+        {
+            const int addedElement = 1;
+            _sortedList.Add(addedElement);
+            const int toRemoveElement = 2;
+            const int expectedSize = 1;
+            _sortedList.Remove(toRemoveElement);
+            Assert.AreEqual(expectedSize, _sortedList.Count, "Removing a non-present element makes the count non correct");
+        }
+    }
+}
